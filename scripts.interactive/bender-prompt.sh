7d50f5030d8f0029d85ea176fefe55bd1f64f6c3
Lital Natan <lital@springames.com>
2022-05-09 11:29:09 +0300

    Enhancing the new prompt with virtues of the old prompt
    Signed-off-by: Lital Natan <lital@springames.com>

--- scripts.interactive/bender-prompt.sh
+++ scripts.interactive/bender-prompt.sh
@@ -14,10 +14,9 @@ GC=32 # Git clean
 GD=31 # Git dirty
 TC=32
 
-prompt_cmd()
-{
-  GB=$(parse_git_branch_clean)
-}
+LONG_PROMPT_MIN_COLUMNS=120
+MAX_DIR_LENGTH_LONG=40
+MAX_DIR_LENGTH_SHORT=40
 
 parse_git_branch_color () {
   if [[ $(git status 2> /dev/null | tail -1) != "nothing to commit, working tree clean" ]]; then
@@ -39,17 +38,38 @@ show_git_prompt () {
   git branch 2>/dev/null 1>&2 && echo -e "-( \e[$(parse_git_branch_color);1m${GB}\e[$MC;1m )"
 }
 
-if [[ -n $(type -t git) ]] ; then
-  PS1="\$(show_git_prompt)"
-else
-  PS1=
-fi
-
 [ "$EUID" -eq 0 ] && MC=$RC
 
-PS1="
-\[\e[$MC;1m\]┌───=[\[\e[$TC;1m\]\t\[\e[$MC;1m\]]=[ \[\e[$UC;1m\]\u\[\e[$MC;1m\]@\[\e[$HC;1m\]\h\[\e[$MC;1m\] ]-( \[\e[39;1m\]\j\[\e[$MC;1m\] )-[ \[\e[39;1m\]\w\[\e[$MC;1m\] ]$PS1
-\[\e[$MC;1m\]└──\\$ \[\e[0m\]"
+GIT_STAT=""
+PS1_LONG="
+\[\e[$MC;1m\]┌─=[\[\e[$TC;1m\]\t\[\e[$MC;1m\]]=[ \[\e[$UC;1m\]\u\[\e[$MC;1m\]@\[\e[$HC;1m\]\h\[\e[$MC;1m\] ]-( \[\e[39;1m\]\j\[\e[$MC;1m\] )-[ \[\e[39;1m\]\$SHORT_DIR\[\e[$MC;1m\] ]\$GIT_STAT
+\[\e[$MC;1m\]└\\$ \[\e[0m\]"
+
+PS1_SHORT="\[\e[$MC;1m\][\[\e[$UC;1m\]\u\[\e[$MC;1m\]@\[\e[$HC;1m\]\h\[\e[$MC;1m\]:\$SHORT_DIR] \\$ \[\e[0m\]"
+
+prompt_cmd()
+{
+  SHORT_DIR=${PWD/$HOME/\~}
+  DIR_LENGTH=$MAX_DIR_LENGTH_LONG
+  if [ "$COLUMNS" -lt $LONG_PROMPT_MIN_COLUMNS ]; then
+     DIR_LENGTH=$MAX_DIR_LENGTH_SHORT
+  fi
+  if [ "${#SHORT_DIR}" -gt "$DIR_LENGTH" ]; then
+    let SHORT_DIR_LEN=$DIR_LENGTH-3
+    SHORT_DIR="...${SHORT_DIR: -$SHORT_DIR_LEN}"
+  fi
+  GB=$(parse_git_branch_clean)
+  if [[ -n $(type -t git) ]] ; then
+    GIT_STAT="$(show_git_prompt)"
+  else
+    GIT_STAT=""
+  fi
+  if [ "$COLUMNS" -lt $LONG_PROMPT_MIN_COLUMNS ]; then
+    PS1="$PS1_SHORT"
+  else
+    PS1="$PS1_LONG"
+  fi
+}
 
 # Display running command in GNU Screen window status
 #
